d6a8d3a78b658956d8ba7d640dd45b8d8d5b6e2a
JamsBak <jigahaganaga@yahoo.com.au>
2023-12-05 19:36:51 +1000

    james go brr

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 import {Pen} from "../lib/monke.js"
 const p = new Pen(preload,setup,draw);
+const shape=p.shape;
+const colour=p.colour;
 let x = 0;
 let img=p.loadImage(0,0,"../images/sample.png");
 let img2=p.loadImage(p.w/2,p.h/2,"../images/sample2.png");
-let btn = p.makeButton(200,0,100,50);
+let btn = p.makeButton(200,100,100,50);
+let btn2 = p.makeButton(350,100,100,50);
+btn.label=`Entity Id:${btn.id}`;
+btn2.label=`Entity Id:${btn2.id}`;
+window.pen=p;
 
 p.preload();
 
@@ -15,14 +21,15 @@ function setup() {
 }
 
 function draw() {
-    p.colour.fill="grey";
-    p.shape.rectangle(0+p.w/2,0,p.w,p.canvas.height);
-    p.colour.fill="red";
-    p.shape.rectangle(x,200,100,50);
-    p.shape.oval(p.mouse.x, p.mouse.y, 50, 30); 
-    p.shape.line(300, 50, 400, 150); 
-    p.shape.multiline(10, 10, 50, 50, 10, 90);
-    p.shape.shape(
+    colour.fill="grey";
+    shape.rectangle(p.w/2,p.h/2,p.w,p.h);
+    colour.fill="red";
+    shape.rectangle(x,200,100,50);
+    shape.rectangle(p.mouse.x,p.mouse.y,100,50);
+    shape.oval(p.mouse.x, p.mouse.y, 50, 30); 
+    shape.line(300, 50, 400, 150); 
+    shape.multiline(10, 10, 50, 50, 10, 90);
+    shape.shape(
         150+x, 100, 
         200+x, 150, 
         150+x, 200, 
@@ -41,6 +48,7 @@ function draw() {
     img2.y=p.mouse.y-img2.h/2;
     img2.draw();
     btn.draw();
+    btn2.draw();
 }
 
 //changing it so images have: an x and y
