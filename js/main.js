daa92dbfb0410ba7d3960711ef3d971f133cd59b
JamesBakerMM <jamesnbaker2@gmail.com>
2023-12-09 16:09:44 +1000

    text and json file loading now works, removed preload and setup as core required functions, no longer necessary

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
 import {Pen} from "../lib/Pen.js"
-const p = new Pen(preload,setup,draw);
-window.pen=p;
+const p = new Pen(draw);
 const shape=p.shape;
 const colour=p.colour;
 const mouse=p.mouse;
 const keyboard=p.kb;
+const text=p.text;
+
+p.start();
+
+window.pen=p;
 
 let x = 0;
 
 let img=p.loadImage(p.mouse.x,p.mouse.y,"../images/sample.png");
-let img2=p.loadImage(p.w/2,p.h/2,"../images/sample2.png");
-
+let img5=p.loadImage(p.mouse.x,p.mouse.y,"../images/sample3.png");
+let img2=p.loadImage(p.w/2,p.h/2,"../images/sample2.png"); 
+let txt=p.loadTextFile("../data/hello.txt");
+let j=p.loadJsonFile("../data/jason.json");
 let btn = p.makeButton(200,100,100,50);
 btn.label=`Entity Id:${btn.id}`;
 
 let btn2 = p.makeButton(350,100,100,50);
 btn2.label=`Entity Id:${btn2.id}`;
 
-p.preload();
-
-function preload(){}
 
 function setup() {
-    p.w=800;
-    p.h=350;    
-    img.x=p.w/2;
-    img.y=200;   
+    if(p.frameCount===0){
+        p.w=800;
+        p.h=350; 
+        img.x=p.w/2;
+        img.y=p.h/2;  
+        img.w=p.w;
+        img.h=p.h;
+        console.log(j)
+    }
 }
 
 function draw() { 
+    setup();
     img.rotation=0;
-    img.w=p.w;
-    img.h=p.h;
+
     colour.fill="grey";
     shape.rectangle(p.w/2,p.h/2,p.w,p.h);
     colour.fill="red";
@@ -47,11 +55,11 @@ function draw() {
         100+x, 150
     );
 
-    p.shape.line(
+    shape.line(
         200,0,
         200,p.h
     )
-    p.text.draw(200,200,"200,200")
+    text.draw(200,200,"200,200")
 
     x += 1;
     if (x > p.w)  {
@@ -62,10 +70,8 @@ function draw() {
 
     img.draw();
 
-    img2.x=200;
-    img2.y=200;
-    img2.x=150;
-    img2.y=150;
+    img2.x=mouse.x;
+    img2.y=mouse.y;
     img2.draw();
     btn.draw();
     btn2.draw();
