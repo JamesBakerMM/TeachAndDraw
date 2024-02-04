994b6e598a885db733e67b313478a0916baa4243
JamesBakerMM <jamesnbaker2@gmail.com>
2024-02-04 15:58:46 +1000

    updated code sample of intention for collision

--- js/stuff/collisionTests.js
+++ js/stuff/collisionTests.js
@@ -1,30 +1,37 @@
-import { $ as p, shp as shape, col as colour, mouse, kb, txt as text } from "../../lib/Pen.js";
+import { $, shp as shape, col as colour, mouse, kb, txt as text } from "../../lib/Pen.js";
 import { Group } from "../../lib/Group.js";
-p.start(draw);
-
+$.start(draw);
+$.debug=true;
 let edges = new Group();
+let squares = new Group();
 
-let leftEdge=p.makeBoxCollider(0,p.h/2,20,p.h);
+let leftEdge=$.makeBoxCollider(0,$.h/2,20,$.h);
 leftEdge.static=true;
-let rightEdge=p.makeBoxCollider(p.w,p.h/2,20,p.h);
+let rightEdge=$.makeBoxCollider($.w,$.h/2,20,$.h);
 rightEdge.static=true;
 
 edges.push(leftEdge);
 edges.push(rightEdge);
 
-let leftSquare=p.makeBoxCollider(p.w/2-20,p.h/2,40,40);
+let leftSquare=$.makeBoxCollider($.w/2-20,$.h/2,40,40);
 leftSquare.velocity.x=2;
-let rightSquare=p.makeBoxCollider(p.w/2+80,p.h/2,40,40);
+let rightSquare=$.makeBoxCollider($.w/2+80,$.h/2,40,40);
 rightSquare.velocity.x=-2;
+squares.push(leftSquare);
+squares.push(rightSquare);
 
 function draw(){
-    leftSquare.collides(rightSquare);
+    if(leftSquare.collides(rightSquare)){
+        console.log("left square collided with right square")
+    }
 
-    leftEdge.collides(leftSquare);
-    edges.collides(leftSquare);
-    leftSquare.draw();
-    rightSquare.draw();
-    leftEdge.draw();
-    rightEdge.draw();
+    if(leftEdge.collides(leftSquare)){
+        console.log("left edge collides with left square")
+    }
 
+    if(edges.collides(rightSquare)){
+        console.log("right square collides with a member of the edges group")
+    }
+    squares.draw();
+    edges.draw();
 }
