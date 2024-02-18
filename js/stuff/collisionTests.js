3db294539208cd8f2b9451c7dcd95b09d35499b8
Andrew Paroz <andyparoz@gmail.com>
2024-02-18 15:01:23 +1000

    Using mass

--- js/stuff/collisionTests.js
+++ js/stuff/collisionTests.js
@@ -13,33 +13,46 @@ rightEdge.static=true;
 //edges.push(leftEdge);
 //edges.push(rightEdge);
 
-let leftSquare=$.makeBoxCollider($.w/2-200,$.h/2-30,40,40);
-leftSquare.velocity.x=1;
-let rightSquare=$.makeBoxCollider($.w/2+200,$.h/2,40,40);
-rightSquare.velocity.x=-1;
+
+let leftSquare=$.makeBoxCollider($.w/2,$.h/2-20,80,80);
+leftSquare.velocity.x = 6;
+leftSquare.mass = 2;
+leftSquare.friction = 10;
 squares.push(leftSquare);
-squares.push(rightSquare);
+
+for (let i = -5; i < 5; i++) {
+    let rightSquare=$.makeBoxCollider($.w/2+200,$.h/2 + i * 60,40,40);
+    rightSquare.velocity.x = 0;
+    rightSquare.mass = 1;
+    rightSquare.friction = 10;
+    squares.push(rightSquare);
+}
+
+for (let i = -5; i < 5; i++) {
+    let rightSquare=$.makeBoxCollider($.w/2+100,$.h/2 + i * 60,40,40);
+    rightSquare.velocity.x = 0;
+    rightSquare.mass = 1;
+    rightSquare.friction = 10;
+    squares.push(rightSquare);
+}
 
 function draw(){
-    if(leftSquare.collides(rightSquare)){
-        console.log("left square collided with right square")
-    }
 
-    //comment out the bellow collides calls
-    //if(leftEdge.collides(leftSquare)){
-    //    console.log("left edge collides with left square")
-    //}
-
-    if(edges.collides(rightSquare)){
-        console.log("right square collides with a member of the edges group")
-    }
+    squares.draw();
+    //edges.draw();
     
     for (let i = 0; i < squares.length; i++) {
+        if(leftSquare.collides(squares[i])){
+            //console.log("left square collided with right square")
+        }
+        for (let j = 0; j < squares.length; j++) {
+            if (i != j) {
+                squares[i].collides(squares[j]);
+            }
+        }
+
         $.colour.fill = "#00000000";
         $.colour.stroke = "#FFFF00FF";
         $.shape.oval(squares[i].x, squares[i].y, squares[i].radius, squares[i].radius);
     }
-
-    squares.draw();
-    //edges.draw();
 }
