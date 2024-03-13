aec497f6ae5528478a8a305607a8cedfbd149a66
Andrew Paroz <andyparoz@gmail.com>
2024-03-13 23:51:14 +1000

    Start of quad tree

--- js/stuff/collisionTests.js
+++ js/stuff/collisionTests.js
@@ -17,9 +17,11 @@ leftSquare.mass = 200000;
 leftSquare.friction = 1;
 squares.push(leftSquare);
 
+let rightSquare;
+
 for (let i = 0; i < 800; i += 15) {
-    for (let j = 0; j < 400; j += 15) {
-        let rightSquare=$.makeBoxCollider(i,j, 5, 5);
+    for (let j = 0; j < 400; j += 10) {
+        rightSquare=$.makeBoxCollider(i,j, 5, 5);
         rightSquare.velocity.x = 0;
         rightSquare.mass = 1;
         rightSquare.friction = 10;
@@ -55,6 +57,26 @@ function draw(){
         leftSquare.velocity.x++
         leftSquare.rotation=0;
     }
+
+    let colorArray = ["white", "yellow", "red", "purple", "blue"];
+
+    /*
+    Quad tree testing stuff
+    let num = $.quadTree.getValue(leftSquare.x, leftSquare.y, leftSquare.w, leftSquare.h);
+    if (true) {
+        for (let i = 1; i <= 4; i++) {
+            for (let j = 1; j <= 4; j++) {
+                for (let k = 1; k <= 4; k++) {
+                    for (let l = 1; l <= 4; l++) {
+                        let q = $.quadTree.getQuad().getQuad(i).getQuad(j).getQuad(k).getQuad(l);
+                        $.colour.fill = colorArray[l];
+                        $.shape.rectangle(q.left + (q.right - q.left)/2, q.top + (q.bottom - q.top)/2, q.size);
+                    }
+                }
+            }
+        }
+    }    
+    */
 
     // if(leftSquare.x>$.width || leftSquare.x<0){
     //     leftSquare.velocity.x=-leftSquare.velocity.x
