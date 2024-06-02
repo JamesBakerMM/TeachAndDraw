a06c488edb06cff57910739b7cf2d18a92f0e92a
JamesBakerMM <jamesnbaker2@gmail.com>
2024-06-02 20:50:18 +1000

    updated group to group test with a static collide r in middle

--- js/stuff/groupToGroupCollisionTest.js
+++ js/stuff/groupToGroupCollisionTest.js
@@ -1,30 +1,37 @@
-import {$} from "../../lib/Pen.js";
+import { $ } from "../../lib/Pen.js";
 
 $.use(draw);
-
-let leftGroup=$.makeGroup(); 
-let rightGroup=$.makeGroup(); 
-for(let i=0; i<3; i++){
-    leftGroup.push(makeLeftShip(400,100+100*i));
-    rightGroup.push(makeRightShip(200,100+100*i));
+let beegSprite = makeBeegSprite($.w / 2, $.h / 2);
+let leftGroup = $.makeGroup();
+let rightGroup = $.makeGroup();
+for (let i = 0; i < 5; i++) {
+    leftGroup.push(makeLeftShip(700, 100 + 100 * i));
+    rightGroup.push(makeRightShip(100, 100 + 100 * i));
 }
-function draw(){
+function draw() {
     leftGroup.collides(rightGroup);
-
+    beegSprite.collides(leftGroup);
+    beegSprite.collides(rightGroup);
     $.drawAllColliders();
 }
 
-function makeLeftShip(x,y){
-    const tempShip = $.makeBoxCollider(x,y,20,20);
-    tempShip.direction=270;
-    tempShip.friction=0;
-    tempShip.speed=20;
+function makeBeegSprite(x, y) {
+    const tempShip = $.makeBoxCollider(x, y, 100, 100);
+    tempShip.static=true;
     return tempShip;
 }
-function makeRightShip(x,y){
-    const tempShip = $.makeBoxCollider(x,y,20,20);
-    tempShip.direction=90;
-    tempShip.friction=0;
-    tempShip.speed=20;
+
+function makeLeftShip(x, y) {
+    const tempShip = $.makeBoxCollider(x, y, 20, 20);
+    tempShip.direction = 270;
+    tempShip.friction = 0;
+    tempShip.speed = 20;
+    return tempShip;
+}
+function makeRightShip(x, y) {
+    const tempShip = $.makeBoxCollider(x, y, 20, 20);
+    tempShip.direction = 90;
+    tempShip.friction = 0;
+    tempShip.speed = 20;
     return tempShip;
 }
