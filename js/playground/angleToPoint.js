57bf4bf1d4fcc187437ad7b706984b687432734b
JamesBakerMM <jamesnbaker2@gmail.com>
2024-07-27 23:09:18 +1000

    added getAngleToPoint

---
+++ js/playground/angleToPoint.js
@@ -0,0 +1,12 @@
+import { $ } from "../../lib/Pen.js";  
+
+$.use(update);
+
+const test = $.makeBoxCollider($.w/2,$.h/2,20,20);
+
+function update() {
+    test.rotation = test.getAngleToPoint($.mouse.x,$.mouse.y);
+    $.shape.line(test.x,test.y,$.mouse.x,$.mouse.y);
+    
+    test.draw();
+}
