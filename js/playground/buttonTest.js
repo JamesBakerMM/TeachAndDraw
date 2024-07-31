4678431931c6dc407757f1389ce1038cc9cefd82
Nick Bland <nick.bland@nickbland.dev>
2024-07-31 15:38:01 +1000

    Add in proper text wrapping for buttons
    Very big. If text would overflow and create a new line, adjust the initial Y position to keep text centred. This may be a bug in the text library file, but my solution works around it.

--- js/playground/buttonTest.js
+++ js/playground/buttonTest.js
@@ -1,15 +1,26 @@
 import { $ } from "../../lib/Pen.js";
 
 $.use(update);
+// $.debug = true;
 
-console.log($.gui.theme);
-$.gui.theme = "grevszfzd";
+// console.log($.text.size);
+// console.log($.gui.theme);
+// $.gui.theme = "grevszfzd";
 
-const btn = $.makeButton($.w / 2, $.h / 2, 200, 100, "jhsdkfjsdh");
+const btn = $.makeButton(
+    $.w / 2,
+    $.h / 2,
+    200,
+    100,
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tempus."
+);
+
+const btn2 = $.makeButton($.w / 2, $.h / 2 + 100, 200, 100, "small text");
 
 function update() {
-    btn.draw();
-    if (btn.released) {
+    if (btn.up) {
         console.log("kjhsdfkjsd");
     }
+    btn.draw();
+    btn2.draw();
 }
