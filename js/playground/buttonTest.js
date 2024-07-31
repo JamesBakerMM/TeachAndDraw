276b064c51eceb66b4cfb7ffbabf5b95500be2b4
Nick Bland <nick.bland@nickbland.dev>
2024-07-31 14:00:32 +1000

    Begin framework for GUI
    🏗️ GUI now initialised inside of pen as its own object. Can be accessed with $.gui. Also added in some basic validation and error messaging when assigning a new theme

--- js/playground/buttonTest.js
+++ js/playground/buttonTest.js
@@ -2,11 +2,14 @@ import { $ } from "../../lib/Pen.js";
 
 $.use(update);
 
-const btn = $.makeButton($.w/2,$.h/2,200,100,"jhsdkfjsdh");
+console.log($.gui.theme);
+$.gui.theme = "grevszfzd";
+
+const btn = $.makeButton($.w / 2, $.h / 2, 200, 100, "jhsdkfjsdh");
 
 function update() {
     btn.draw();
-    if(btn.released){
+    if (btn.released) {
         console.log("kjhsdfkjsd");
     }
 }
