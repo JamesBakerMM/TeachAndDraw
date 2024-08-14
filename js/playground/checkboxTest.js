171a942b029982af265db2bc05f08885e9927286
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-08-14 17:09:57 +1000

    Add clickable area around checkbox

--- js/playground/checkboxTest.js
+++ js/playground/checkboxTest.js
@@ -1,14 +1,15 @@
 import { $ } from "../../lib/Pen.js";
 
 $.use(update);
-
+$.gui.accentColour = "red";
 const checkbox = $.makeCheckbox(    
     $.w / 2,
     $.h / 2,
-    20
+    15
 );
-checkbox.value = 9;
+// checkbox.checked = "true";
 
 function update() {
     checkbox.draw();
+    $.makeCh
 }
