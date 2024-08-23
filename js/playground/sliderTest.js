f0c15aa410b7daee87110f6855395c329b2f3d04
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-08-23 15:38:08 +1000

    Add restriction that step must be an integer

--- js/playground/sliderTest.js
+++ js/playground/sliderTest.js
@@ -1,17 +1,6 @@
 import { $ } from "../../lib/Pen.js";
 
 $.use(update);
-const slider = $.makeSlider(    
-    $.w / 2,
-    $.h / 2,
-    100
-);
-slider.min = 10;
-console.log(slider);
-
-function update() {
-    slider.draw();
-}
 
 // test widths, test steps
 // test marks
@@ -19,3 +8,45 @@ function update() {
 // test steps correct/incorrect
 // test the different numbers in the popup
 // test turning all parts on/off
+const slider = $.makeSlider(    
+    $.w / 2,
+    $.h / 2,
+    350
+);
+// slider.value = 110;
+// slider.max = 100;
+slider.min = -10;
+slider.value = 50;
+slider.max = 100;
+
+const sliderGroup1 = $.makeGroup();
+for (let i = 1; i < 7; i++) {
+    const slider = $.makeSlider($.w/2 - 230, 50 * i + 50, 50 * i + 10);
+    slider.min = -((10) ** i);
+    slider.value = 0;
+    slider.max = (10) ** i;
+    slider.step = slider.max / 10;
+    sliderGroup1.push(slider);
+}
+
+const sliderGroup2 = $.makeGroup();
+for (let i = 1; i < 7; i++) {
+    const slider = $.makeSlider($.w/2 - 230, 50 * i + 50, 50 * i + 10);
+    slider.min = -((10) ** i);
+    slider.value = 0;
+    slider.max = (10) ** i;
+    slider.step = slider.max / 10;
+    sliderGroup2.push(slider);
+}
+
+
+function update() {
+    // for(const box of boxGroup){
+    //     if(box.name==="red" && box.checked){
+    //         $.colour.fill=boxGroup[0].value;
+    //         $.shape.rectangle($.w/2,$.h/2,$.w,$.h);
+    //     }
+    // }
+    sliderGroup1.draw();
+    sliderGroup2.draw();
+}
