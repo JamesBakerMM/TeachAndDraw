65263d6f83d8027d46ccd81b814bff51d1bef954
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-08-28 17:33:43 +1000

    Add options property

--- js/playground/checkboxTest.js
+++ js/playground/checkboxTest.js
@@ -14,7 +14,6 @@ for(let i=0; i<10; i++) {
 // const dropdown = $.makeDropdown(100, 200, 50, 20, ["option 1", 3, "option 3", true, ["hi", "hello"], {}]);
 const dropdown = $.makeDropdown(100, 200, 50, 20, ["option 1", "option 2 :)", "option 3"]);
 
-
 boxGroup[0].name="red";
 boxGroup[0].value="red";
 boxGroup[0].accentColour="red";
