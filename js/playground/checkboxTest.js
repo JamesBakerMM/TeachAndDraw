12c06e029b0569b09d0cf375167309d29dbc38da
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-08-30 12:56:25 +1000

    Add value set/get

--- js/playground/checkboxTest.js
+++ js/playground/checkboxTest.js
@@ -25,6 +25,7 @@ for(let i=0; i<10; i++) {
 // const dropdown = $.makeDropdown(100, 200, 50, 20, ["option 1", 3, "option 3", true, ["hi", "hello"], {}]);
 const dropdown = $.makeDropdown(100, 200, 50, 20, ["option 1", "option 2 :)", "option 3"]);
 dropdown.index = 2;
+dropdown.value = "option 1";
 
 boxGroup[0].name="red";
 boxGroup[0].value="red";
