a47d8b61397a23c5341c09e1b53722c7710a0443
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-09-03 13:22:31 +1000

    Draw button with up/down arrows

--- js/playground/checkboxTest.js
+++ js/playground/checkboxTest.js
@@ -23,8 +23,9 @@ for(let i=0; i<10; i++) {
 }
 
 // const dropdown = $.makeDropdown(100, 200, 50, 20, ["option 1", 3, "option 3", true, ["hi", "hello"], {}]);
-const dropdown = $.makeDropdown(400, 200, 150, 20, ["option 1", "option 2 :)", "option 3"]);
+const dropdown = $.makeDropdown(400, 200, 300, 30, ["option 1", "option 2 :)", "option 3"]);
 dropdown.open = true;
+// dropdown.openDirection = "down";
 
 
 boxGroup[0].name="red";
