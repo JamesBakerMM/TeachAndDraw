ac2d2f6f2d5fb5ba6d76741cd4ef09e8fb712b4c
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-09-03 13:52:58 +1000

    Draw basic options

--- js/playground/checkboxTest.js
+++ js/playground/checkboxTest.js
@@ -23,7 +23,7 @@ for(let i=0; i<10; i++) {
 }
 
 // const dropdown = $.makeDropdown(100, 200, 50, 20, ["option 1", 3, "option 3", true, ["hi", "hello"], {}]);
-const dropdown = $.makeDropdown(400, 200, 300, 30, ["option 1", "option 2 :)", "option 3"]);
+const dropdown = $.makeDropdown(400, 200, 300, 30, ["option 1", "option 2 :)", "option 3", "Another option", "Wow! Another option. Cool, cool cool."]);
 dropdown.open = true;
 // dropdown.openDirection = "down";
 
