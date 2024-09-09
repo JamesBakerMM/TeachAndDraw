5697315afb65996dc13beb2a4da1e6f18547fe43
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-09-09 15:18:47 +1000

    Change debug class to use string in text print

--- js/playground/textTest.js
+++ js/playground/textTest.js
@@ -25,5 +25,30 @@ function update() {
     slider.draw();
 	$.text.print(400, 300, "Testing!");
 	$.text.print(400, 350, "Testing with max width!", 100);
-	$.text.print(400, 200, 234);
+	// $.text.print(400, 200, 234);
+
+	$.text.alignment.y = "top";
+	$.text.alignment.x = "left";
+	$.text.print(100, 50, "Top Left");
+	$.text.alignment.x = "center";
+	$.text.print(100, 80, "Top Center");
+	$.text.alignment.x = "right";
+	$.text.print(100, 110, "Top Right");
+
+	$.text.alignment.y = "center";
+	$.text.alignment.x = "left";
+	$.text.print(100, 160, "Center Left");
+	$.text.alignment.x = "center";
+	$.text.print(100, 190, "Center Center");
+	$.text.alignment.x = "right";
+	$.text.print(100, 220, "Center Right");
+
+
+	$.text.alignment.y = "bottom";
+	$.text.alignment.x = "left";
+	$.text.print(100, 270, "Bottom Left");
+	$.text.alignment.x = "center";
+	$.text.print(100, 300, "Bottom Center");
+	$.text.alignment.x = "right";
+	$.text.print(100, 330, "Bottom Right");
 }
