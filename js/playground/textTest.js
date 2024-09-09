0a0aa1ae46bd2f45498d50742cb9c045309a7317
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-09-09 16:20:35 +1000

    Add more text wrap testing

--- js/playground/textTest.js
+++ js/playground/textTest.js
@@ -17,8 +17,9 @@ const button = $.makeButton(
     $.w/2-200,
     $.h/2,
     100,
-    50
-)
+    50,
+	"Multiple lined button!"
+) //test "Multiple-lined button!". For some reason it starts the text on the second line if it cant fit in the first line
 
 function update() {
     button.draw();
@@ -35,6 +36,9 @@ function update() {
 	$.text.alignment.x = "right";
 	$.text.print(100, 110, "Top Right");
 
+	//testing wrapping
+	$.text.print(450, 50, "Y alignment is top, X is right, this is how it wraps", 150);
+
 	$.text.alignment.y = "center";
 	$.text.alignment.x = "left";
 	$.text.print(100, 160, "Center Left");
@@ -43,6 +47,9 @@ function update() {
 	$.text.alignment.x = "right";
 	$.text.print(100, 220, "Center Right");
 
+	//testing wrapping
+	$.text.alignment.x = "left";
+	$.text.print(450, 150, "Y alignment is center, X is left, this is how it wraps", 150);
 
 	$.text.alignment.y = "bottom";
 	$.text.alignment.x = "left";
@@ -51,4 +58,9 @@ function update() {
 	$.text.print(100, 300, "Bottom Center");
 	$.text.alignment.x = "right";
 	$.text.print(100, 330, "Bottom Right");
+
+	//testing wrapping
+	$.text.alignment.x = "center";
+	$.text.print(450, 250, "Y alignment is bottom, X is center, this is how it wraps", 150);
+
 }
