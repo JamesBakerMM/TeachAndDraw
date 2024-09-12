9ff7efab62f0a17f7a9b65ac7da938d14718870b
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-09-12 15:47:49 +1000

    Add basic 2 layer hyphenation

--- js/playground/textTest.js
+++ js/playground/textTest.js
@@ -64,6 +64,6 @@ function update() {
 	$.text.alignment.x = "center";
 	// $.text.print(450, 250, "Y alignment is bottom, X is center, this is how it wraps", 150);
 
-	$.text.print(400, 550, "Incomprehensible and incalculable monstrocities with antidisestablishmentarianism", 100);
+	$.text.print(400, 500, "Incomprehensible and incalculable monstrocities with antidisestablishmentarianism", 100);
 
 }
