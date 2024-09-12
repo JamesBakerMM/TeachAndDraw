386d9cdcf7dc7fb0523f6d077cb05d512405ca96
brit12345 <93416463+brit12345@users.noreply.github.com>
2024-09-12 15:41:18 +1000

    Implement basic hyphen words

--- js/playground/textTest.js
+++ js/playground/textTest.js
@@ -22,48 +22,48 @@ const button = $.makeButton(
 ) //test "Multiple-lined button!". For some reason it starts the text on the second line if it cant fit in the first line
 
 function update() {
-    button.draw();
-    slider.draw();
-	$.text.print(400, 300, "Testing!");
-	$.text.print(400, 350, "Testing with max width!", 100);
-	// $.text.print(400, 200, 234);
+    // button.draw();
+    // slider.draw();
+	// $.text.print(400, 300, "Testing!");
+	// $.text.print(400, 350, "Testing with max width!", 100);
+	// // $.text.print(400, 200, 234);
 
-	$.text.alignment.y = "top";
-	$.text.alignment.x = "left";
-	$.text.print(100, 50, "Top Left");
-	$.text.alignment.x = "center";
-	$.text.print(100, 80, "Top Center");
-	$.text.alignment.x = "right";
-	$.text.print(100, 110, "Top Right");
+	// $.text.alignment.y = "top";
+	// $.text.alignment.x = "left";
+	// $.text.print(100, 50, "Top Left");
+	// $.text.alignment.x = "center";
+	// $.text.print(100, 80, "Top Center");
+	// $.text.alignment.x = "right";
+	// $.text.print(100, 110, "Top Right");
+
+	// //testing wrapping
+	// $.text.print(450, 50, "Y alignment is top, X is right, this is how it wraps", 150);
+
+	// $.text.alignment.y = "center";
+	// $.text.alignment.x = "left";
+	// $.text.print(100, 160, "Center Left");
+	// $.text.alignment.x = "center";
+	// $.text.print(100, 190, "Center Center");
+	// $.text.alignment.x = "right";
+	// $.text.print(100, 220, "Center Right");
+
+	// //testing wrapping
+	// $.text.alignment.x = "left";
+	// $.text.print(450, 150, "Y alignment is center, X is left, this is how it wraps", 150);
+	// $.text.print(550, 500, "Y alignment is center, X is left, this is how it wraps", 100);
+
+	// $.text.alignment.y = "bottom";
+	// $.text.alignment.x = "left";
+	// $.text.print(100, 270, "Bottom Left");
+	// $.text.alignment.x = "center";
+	// $.text.print(100, 300, "Bottom Center");
+	// $.text.alignment.x = "right";
+	// $.text.print(100, 330, "Bottom Right");
 
 	//testing wrapping
-	$.text.print(450, 50, "Y alignment is top, X is right, this is how it wraps", 150);
-
-	$.text.alignment.y = "center";
-	$.text.alignment.x = "left";
-	$.text.print(100, 160, "Center Left");
 	$.text.alignment.x = "center";
-	$.text.print(100, 190, "Center Center");
-	$.text.alignment.x = "right";
-	$.text.print(100, 220, "Center Right");
+	// $.text.print(450, 250, "Y alignment is bottom, X is center, this is how it wraps", 150);
 
-	//testing wrapping
-	$.text.alignment.x = "left";
-	$.text.print(450, 150, "Y alignment is center, X is left, this is how it wraps", 150);
-	$.text.print(550, 500, "Y alignment is center, X is left, this is how it wraps", 100);
-
-	$.text.alignment.y = "bottom";
-	$.text.alignment.x = "left";
-	$.text.print(100, 270, "Bottom Left");
-	$.text.alignment.x = "center";
-	$.text.print(100, 300, "Bottom Center");
-	$.text.alignment.x = "right";
-	$.text.print(100, 330, "Bottom Right");
-
-	//testing wrapping
-	$.text.alignment.x = "center";
-	$.text.print(450, 250, "Y alignment is bottom, X is center, this is how it wraps", 150);
-
-	$.text.print(400, 550, "longsinglewordtoolong and now little words", 100);
+	$.text.print(400, 550, "Incomprehensible and incalculable monstrocities with antidisestablishmentarianism", 100);
 
 }
