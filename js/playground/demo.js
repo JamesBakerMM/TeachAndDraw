2eef6435d49ec1e648bc2985d3663800a504c91f
JamesBakerMM <jamesnbaker2@gmail.com>
2024-10-26 11:25:15 +1000

    fix get accessing .speed should reverse engineer the #speed value based on the current velocities. #111

--- js/playground/demo.js
+++ js/playground/demo.js
@@ -1,7 +1,9 @@
 import { $ } from "../../lib/Pen.js";
 
 $.use(update);
-            
+
+const ball = $.loadImage(200,200,"../examples/pachinko/images/ball.png");
+
 function update() {
-        
+    ball.draw();
 }
