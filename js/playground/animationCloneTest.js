ea076fc7c0baa2977bd57b6ae913874049192c1b
mellic03 <mellic03@gmail.com>
2024-11-14 14:26:41 +1000

    Initial commit

---
+++ js/playground/animationCloneTest.js
@@ -0,0 +1,33 @@
+import { $ } from "../../lib/Pen.js"
+
+$.use(draw);
+$.width  = 816;
+$.height = 900;
+
+let A, B, C;
+
+function preload() {
+    const paths = [];
+
+    for (let i=0; i<=9; i++)
+    {
+        paths.push(`images/explosion/explosion${i}.png`);
+    }
+
+    A = $.loadAnimation(100, 100, ...paths);
+    A.scale = 200;
+    A.looping = true;
+    A.rotation = 45;
+
+    B = A.clone($, 200, 100, A.frames);
+    C = B.clone($, 300, 100, B.frames);
+}
+
+preload();
+
+
+function draw() {
+
+    A.draw();
+    B.draw();
+}
