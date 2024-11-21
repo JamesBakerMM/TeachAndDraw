68b9f5a479651922b2dc22e02aa096924a671f5f
mellic03 <mellic03@gmail.com>
2024-11-21 12:18:41 +1000

    Text rotation

--- js/playground/textRotationTest.js
+++ js/playground/textRotationTest.js
@@ -4,11 +4,26 @@ $.use(draw);
 $.width  = 800;
 $.height = 800;
 
+let theta   = 0;
+let size    = 16;
+let sizedir = 1;
 
 function draw() {
 
-    $.text.print(100, 100, "Test");
-    $.text.rotation = 45;
-    $.text.print(200, 100, "Test");
+    $.text.rotation = theta;
+    $.text.print(100, 100, "Rotating text!", 48);
+    $.text.print(250, 100, "Rotating text!");
 
+    
+    if (size <= 16)      sizedir = +0.2;
+    else if (size >= 32) sizedir = -0.2;
+    size += sizedir;
+
+    $.text.size = size;
+    $.text.print(400, 100, "Rotating text!", 96);
+    $.text.size = 16;
+
+
+
+    theta += 1;
 }
