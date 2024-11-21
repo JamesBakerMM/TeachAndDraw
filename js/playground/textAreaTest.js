01139b12e675a8a5bc77fc5f02e6cc2cc75050b0
mellic03 <mellic03@gmail.com>
2024-11-22 08:54:02 +1000

    Initial changes

---
+++ js/playground/textAreaTest.js
@@ -0,0 +1,17 @@
+import { $ } from "../../lib/Pen.js";
+// $.debug = true;
+
+$.use(update);
+
+
+const textArea = $.makeTextArea(400, 200, 300, 150);
+textArea.value = `Write your great message here...`;
+
+function update() {
+
+	textArea.draw();
+	textArea.characterLimit = 300;
+
+
+
+}
