34120ef0932eb4c513236cead6409e3dd212ffc9
mellic03 <mellic03@gmail.com>
2024-11-22 10:37:25 +1000

    Improvements

--- js/playground/textAreaTest.js
+++ js/playground/textAreaTest.js
@@ -10,6 +10,23 @@ const textArea = $.makeTextArea(256, 256, 300, 300);
 textArea.value = `Write your great message here...`;
 
 function update() {
+	$.text.alignment.x = "left";
+	$.text.alignment.y = "top";
+
+	$.text.print(0, 0, "TextArea now supports:");
+	$.text.print(0, 20,  "- ctl + A");
+	$.text.print(0, 40,  "- ctl + C");
+	$.text.print(0, 60,  "- ctl + X");
+	$.text.print(0, 80, "- ctl + V");
+	$.text.print(0, 100, "- ctl + A");
+	$.text.print(0, 120, "- ctl + backspace");
+	$.text.print(0, 140, "- ctl + left/right");
+
+	$.text.print(175, 20, "- shift + left/right");
+	$.text.print(175, 40, "- shift + home/end");
+
+	$.text.print(350, 20,  "- home/end");
+
 
 	textArea.draw();
 	textArea.characterLimit = 300;
