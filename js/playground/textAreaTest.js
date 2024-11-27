1b997d629c8b252e6ed9316a9282b9a40a26a23d
mellic03 <mellic03@gmail.com>
2024-11-27 23:23:59 +1000

    Initial commit

--- js/playground/textAreaTest.js
+++ js/playground/textAreaTest.js
@@ -9,6 +9,29 @@ $.height = 512;
 const textArea = $.makeTextArea(256, 256, 300, 300);
 textArea.value = `Write your great message here...`;
 
+
+function testRee() {
+	throw Error("Rip");
+}
+
+
+class Bitch {
+	constructor() {
+		this.data = [];
+	}
+
+	getData( i ) {
+		if (0 <= i && i<= this.data.length-1) {
+			return this.data[i];
+		} else {
+			throw RangeError("Out of bounds access");
+		}
+	}
+}
+
+const B = new Bitch;
+
+
 function update() {
 	$.text.alignment.x = "left";
 	$.text.alignment.y = "top";
@@ -28,7 +51,9 @@ function update() {
 
 	$.text.print(350, 20,  "- home/end");
 
-
+	B.getData(305)
+	
+	
 	textArea.draw();
 	textArea.characterLimit = 300;
 
