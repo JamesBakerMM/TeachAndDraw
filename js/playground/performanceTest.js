97b3f649bd6a131bb27577e9c5c572f3a986a89b
mellic03 <mellic03@gmail.com>
2024-12-01 01:34:27 +1000

    PerformanceMetrics.js

--- js/playground/performanceTest.js
+++ js/playground/performanceTest.js
@@ -1,6 +1,6 @@
 import { $ } from "../../lib/Pen.js";
-import { Performance } from "../../lib/Performance.js";
-// $.debug = true;
+import { PerformanceMetrics } from "../../lib/PerformanceMetrics.js";
+$.debug = false;
 
 $.use(update);
 $.width  = 512;
@@ -20,8 +20,8 @@ setup();
 
 function update() {
 
-    const G0 = $.makeGroup();
-    const A = $.makeCircleCollider(256, 256, 32);
+    // const G0 = $.makeGroup();
+    // const A = $.makeCircleCollider(256, 256, 32);
 
 
     $.drawColliders();
