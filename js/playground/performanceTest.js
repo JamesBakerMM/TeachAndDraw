300d2363a42af34a0b047fb7479c368fc90be0a5
mellic03 <mellic03@gmail.com>
2024-12-02 22:14:00 +1000

    Added generic performance metrics

--- js/playground/performanceTest.js
+++ js/playground/performanceTest.js
@@ -1,28 +1,49 @@
 import { $ } from "../../lib/Pen.js";
-import { PerformanceMetrics } from "../../lib/PerformanceMetrics.js";
-$.debug = true;
+import { MetricBase } from "../../lib/PerformanceMetrics.js";
 
+
+// Example performance metric.
+// -------------------------------------------------
+class MetricTest extends MetricBase {
+    measurePerformance() {
+        return {value: $.math.random(0, 100)};
+    };
+
+    computeTrends( history ) {
+        let avg = 0;
+
+        for (let data of history) {
+            avg += data.value
+        }
+
+        avg /= history.length;
+
+        return {value: avg};
+    };
+
+    printMeasurement( measurement ) {
+        console.groupCollapsed("Test Metric");
+        console.log(`Value: ${measurement.value}`);
+        console.groupEnd("Test Metric");
+    };
+
+    printTrends( trends ) {
+        this.printMeasurement(trends);
+    };
+}
+
+$.performanceMetrics.addMetric(new MetricTest());
+// -------------------------------------------------
+
+
+
+
+$.debug = true;
 $.use(update);
 $.width  = 512;
 $.height = 512;
 
 
-
-function setup()
-{
-    // Performance.collisionGroupFasterThanArray($);
-}
-
-setup();
-
-
-
-
 function update() {
 
-    // const G0 = $.makeGroup();
-    // const A = $.makeCircleCollider(256, 256, 32);
-
-
-    $.drawColliders();
 }
