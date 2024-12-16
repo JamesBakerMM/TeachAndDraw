27de9f207d9df7412e696db57afc6e9c192b7a36
mellic03 <mellic03@gmail.com>
2024-12-16 22:11:29 +1000

    Initial changes

--- js/playground/soundDemo.js
+++ js/playground/soundDemo.js
@@ -5,7 +5,8 @@ const exampleSound = $.loadSound("../../data/test.mp3");
 
 function update(){
     if(exampleSound.isPlaying===false){
-        exampleSound.play();
+exampleSound.startTime = 19;
+exampleSound.play();
     } 
 
 }
