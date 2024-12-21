94ec9b942948e5654f5c741a1d43ada83f13fbc6
mellic03 <mellic03@gmail.com>
2024-12-22 01:28:29 +1000

    Refactoring to better utilise .update

--- js/playground/lifespanTest.js
+++ js/playground/lifespanTest.js
@@ -1,74 +1,107 @@
 import { $ } from "../../lib/Pen.js"
+import { Button } from "../../lib/Button.js";
+import { Checkbox } from "../../lib/Checkbox.js";
+import { Slider } from "../../lib/Slider.js";
+import { Dropdown } from "../../lib/Dropdown.js";
+import { Entity } from "../../lib/Entity.js";
+import { MovingStamp } from "../../lib/Animation.js";
 
 $.use(draw);
 $.width  = 816;
 $.height = 900;
 
+const COLS = 5;
 let lifespans = [];
 
-let anims = [];
-
-let checkboxes = [];
-let buttons = [];
-let sliders = [];
+/** @type {MovingStamp[]} */ let anims = [];
+/** @type {Checkbox[]}    */ let checkboxes = [];
+/** @type {Button[]}      */ let buttons = [];
+/** @type {Slider[]}      */ let sliders = [];
+/** @type {Dropdown[]}    */ let dropdowns = [];
 
 let balls;
 let walls;
 
-const COLS = 5;
+let yoffset = 50;
+
 
 
 function createLifespans() {
-    for (let i=0; i<COLS; i++) {
+    for (let i=0; i<COLS-1; i++) {
         lifespans.push(5 + 5*i);
     }
+
+    lifespans.push(Infinity);
 }
 
 function createAnimations() {
     const paths = [];
-
-    for (let i=0; i<=9; i++)
-    {
+    for (let i=0; i<=9; i++) {
         paths.push(`images/explosion/explosion${i}.png`);
     }
-
     for (let i=0; i<COLS; i++) {
-        const A = $.loadAnimation(0, 0, ...paths);
-
+        const A = $.loadAnimation(50+150*i, yoffset, ...paths);
         A.lifespan = lifespans[i];
         A.scale = 200;
         A.looping = true;
         A.duration = 1.0;
-
-        A.x = 100 + 150*i;
-        A.y = 50;
-    
         anims.push(A);
     }
+    yoffset += 100;
 }
 
 function createCheckboxes() {
     for (let i=0; i<COLS; i++) {
-        let cb = $.makeCheckbox(50+150*i, 150, 32);
-        cb.lifespan = lifespans[i];
-        checkboxes.push(cb);
+        const B = $.makeCheckbox(50+150*i, yoffset, 32);
+        B.lifespan = lifespans[i];
+        B.checked = (i % 2 == 1);
+        checkboxes.push(B);
     }
+    yoffset += 100;
 }
 
 function createButtons() {
     for (let i=0; i<COLS; i++) {
-        const B = $.makeButton(50+150*i, 250, 72, 32, String(`Button ${i}`));
+        const B = $.makeButton(50+150*i, yoffset, 72, 32, String(`Button ${i}`));
         B.lifespan = lifespans[i];
         buttons.push(B);
     }
+    yoffset += 100;
 }
 
 function createSliders() {
     for (let i=0; i<COLS; i++) {
-        const S = $.makeSlider(50+150*i, 350, 72);
+        const S = $.makeSlider(50+150*i, yoffset, 72);
         S.lifespan = lifespans[i];
         buttons.push(S);
     }
+    yoffset += 100;
+}
+
+function createDropdowns() {
+    const A = $.makeDropdown(75+0*150, yoffset, 128, [
+        "Option A", "Option B", "Option C", "Option D"
+    ]);
+
+    const B = $.makeDropdown(75+1*150, yoffset, 128, [
+        "Option X", "Option Y", "Option Z", "Option W"
+    ]);
+
+    const C = $.makeDropdown(75+2*150, yoffset, 128, [
+        "Option 1", "Option 2", "Option 3", "Option 4"
+    ]);
+
+    // A.lifespan = 2;
+    // B.lifespan = 4;
+    // C.lifespan = Infinity;
+
+    A.openDirection = "down";
+    B.openDirection = "down";
+    C.openDirection = "up";
+
+    dropdowns.push(A, B, C);
+
+    yoffset += 300;
 }
 
 function createColliders() {
@@ -76,95 +109,103 @@ function createColliders() {
     walls = $.makeGroup();
 
     const bot = $.makeBoxCollider($.width/2, $.height, $.width, 50);
-    const left = $.makeBoxCollider(0, $.height/2+250, 50, $.height-250);
-    const right = $.makeBoxCollider($.width, $.height/2+250, 50, $.height-250);
+    const left = $.makeBoxCollider(0, $.height/2+400, 50, $.height-400);
+    const right = $.makeBoxCollider($.width, $.height/2+400, 50, $.height-400);
     bot.static = true;
     left.static = true;
     right.static = true;
     walls.push(bot, left, right);
 
     for (let i=0; i<COLS; i++) {
-        const B = $.makeCircleCollider(100+150*i, 500, 32);
+        const B = $.makeCircleCollider(100+150*i, yoffset, 32);
         B.lifespan = lifespans[i];
         B.velocity.x = 64 * (Math.random() - 0.5);
         B.mass = 64
         B.friction = 0;
         balls.push(B);
     }
+
+    yoffset += 100;
 }
 
 
 
-
-
-
-function drawAnimations() {
-    $.text.alignment.x = "left";
-    $.text.alignment.y = "top";
-
-    for (const A of anims) {
-        if (A.exists) {
-            A.draw();
-            $.text.print(A.x, A.y+50, String(`lifespan=${Math.round(A.lifespan)}s`));
+/**
+ * @param {Array<Entity>} arr
+ */
+function drawLabelled( arr ) {
+    for (let i=0; i<arr.length; i++) {
+        const A = arr[i];
+        if (A.exists === false) {
+            delete arr[i];
+            arr.splice(i, 1);
+            return;
         }
+
+        let text = `lifespan=${A.lifespan.toFixed(1)}`;
+        if (A.lifespan < Infinity) {
+            text += "s";
+        }
+
+        A.draw();
+        $.text.print(A.x, A.y+25, text);
     }
 }
 
 function drawCheckboxes() {
-    for (const cb of checkboxes) {
-        if (cb.exists) {
-            cb.draw();
-            $.text.print(cb.x, cb.y+25, String(`lifespan=${Math.round(cb.lifespan)}s`));
-        }
+    for (let i=0; i<checkboxes.length; i++) {
+        const B = checkboxes[i];
+        B.ageless = !B.checked;
     }
+    drawLabelled(checkboxes);
 }
 
 function drawButtons() {
     for (const B of buttons) {
-        if (B.exists) {
-            B.draw();
-            $.text.print(B.x, B.y+25, String(`lifespan=${Math.round(B.lifespan)}s`));
+        if (B.exists === false) {
+            continue;
+        }
+    
+        B.label = String(B.lifespan.toFixed(1)) + "s";
+
+        if (B.up) {
+            B.lifespan += 5;
         }
     }
+    drawLabelled(buttons);
 }
 
-function drawSliders() {
-    for (const S of sliders) {
-        if (S.exists) {
-            S.draw();
-            $.text.print(S.x, S.y+25, String(`lifespan=${Math.round(S.lifespan)}s`));
-        }
-    }
-}
 
 function drawColliders() {
     balls.collides(balls);
     balls.collides(walls);
 
-    for (const B of balls) {
-        if (B.exists) {
-            B.velocity.y += 1;
-            $.text.print(B.x, B.y, String(`lifespan=${Math.round(B.lifespan)}s`));
-        }
+    for (let B of balls) {
+        B.velocity.y += 1;
     }
 
     $.drawColliders();
+    drawLabelled(balls);
 }
+
 
 createLifespans();
 createAnimations();
-createColliders();
 createCheckboxes();
 createButtons();
 createSliders();
+createDropdowns();
+createColliders();
 
-// let collider = $.makeCircleCollider($.width/2, $.height/2, 100, 100);
-// collider.asset = anims[0];
 function draw() {
-    drawAnimations();
-    drawColliders();
+    $.text.alignment.x = "left";
+    $.text.alignment.y = "top";
+
+    drawLabelled(anims);
     drawCheckboxes();
     drawButtons();
-    drawSliders();
+    drawLabelled(sliders);
+    drawLabelled(dropdowns);
+    drawColliders();
 
 }
