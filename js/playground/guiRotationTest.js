90aa5cf3e8b3ef8bc309bfdc46415faad19c36bf
mellic03 <mellic03@gmail.com>
2025-04-03 06:14:34 +1000

    gui rotation

--- js/playground/guiRotationTest.js
+++ js/playground/guiRotationTest.js
@@ -5,6 +5,8 @@ $.use(update);
 const btn = $.makeButton(250, 150, 300, 50, "rotating");
 const slider = $.makeSlider($.w-250, 150, 200);
 const checkbox = $.makeCheckbox($.w/2, $.h/2, 100, 100);
+checkbox.checked = true;
+
 const dropdown = $.makeDropdown(250, $.h-150, 200, ["transparent", "red", "blue", "green", "orange", "yellow", "purple", "pink"]);
 const dropup = $.makeDropdown($.w-250, $.h-150, 200, ["This is a dropup. The arrow indicates whether it opens upwards or downwards", "option 2 :) this is also a longer option, causing truncating", "option 3", "Another option", "Wow! Another option. Cool, cool cool. Love that for me", "Wow, a working dropdown", "Very cool", "actually", "it seems perfectly fine! Excellent"]);
 dropup.openDirection = "up";
