e4c75183034ae9a56954a908c0b7f41f89a005f9
mellic03 <mellic03@gmail.com>
2025-04-16 23:05:44 +1000

    button testing

--- js/playground/buttonTest.js
+++ js/playground/buttonTest.js
@@ -36,7 +36,7 @@ function update() {
     // btn2.draw(); // btn.up/down works regardless of draw order.
     $.text.alignment.x = "right";
     $.text.colour = "white";
-    $.text.print(btn2.x-btn2.w/2, btn2.y, "Invisible button here --> ");
+    $.text.print(btn2.x-btn2.w/2, btn2.y, "Button without .draw() call here --> ");
 
     if (btn3.hovered) {
         btn3.label = "hovered";
