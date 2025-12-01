3ac0d80be5dcf0eb98828f485b99719d53b876ce
JamesBakerMM <jamesnbaker2@gmail.com>
2025-12-01 13:05:25 +1000

    Updated for better error feedback, and tweaked colours

---
+++ js/playground/arrayErrorTest.js
@@ -0,0 +1,10 @@
+import { tad, make } from "../../lib/TeachAndDraw.js"
+
+tad.use(update)
+
+tad.debug = true
+const test = make.group();
+
+function update() {
+    // Student *thinks* all rows have length 2
+}
